12b20ce3981fc723049fbd258054e1f4b4885da8
hiroshi-kato <hiroshikato1208@gmail.com>
2022-02-16 09:15:22 +0900

    比較用のコードを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import { useChecks } from './hooks/useChecks'
+import { useState } from 'react';
+import { Checks } from './components/Checks';
+import { useChecks } from './hooks/useChecks';
 
 function App() {
-  const [isAllChecked, AlphabetChecks] = useChecks(['a', 'b', 'c'])
+  // カスタムフック化
+  const [isAllChecked, AlphabetChecks] = useChecks(['a', 'b', 'c']);
+
+  // カスタムフック化しない場合
+  // Checks用コードStart
+  const labels = ['d', 'e', 'f'];
+  const [checkList, setCheckList] = useState(labels.map(() => false));
+  const handleCheckClick = (index: number) => {
+    setCheckList((checks) => checks.map((c, i) => (i === index ? !c : c)));
+  };
+  const isAllChecked2 = checkList.every((x) => x);
+  // Checks用コード End
 
   return (
     <div>
       <p>好きなアルファベットをチェックしてください。</p>
       <AlphabetChecks />
       <button disabled={!isAllChecked}>次へ</button>
+      <Checks
+        checkList={checkList}
+        labels={labels}
+        onCheck={handleCheckClick}
+      />
+      <button disabled={!isAllChecked2}>次へ2</button>
     </div>
   );
 }
 
-export default App
+export default App;
